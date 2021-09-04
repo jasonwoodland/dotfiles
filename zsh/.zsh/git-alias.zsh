2e9b904c3642b2bbba23e8ba1ec5aea97686d3ae
Jason Woodland <me@jasonwoodland.com>
2021-09-04 11:54:43 +1000

    feat(zsh): auto-generate git aliases

--- zsh/.zsh/git-alias.zsh
+++ zsh/.zsh/git-alias.zsh
@@ -1,49 +1,23 @@
 alias g='git'
-alias ga='git add'
-alias gaa='git add --all'
-alias gap='git add --patch'
-alias gb='git branch'
-alias gbl='git branch --list'
-alias gbc='git branch --show-current'
-alias gbm='git branch --move'
-alias gca='git commit --amend'
-alias gcam="git commit --all -m"
-alias gcan='git commit --amend --no-edit'
-alias gc="git commit"
-alias gcm="git commit -m"
-alias gcl='git clean'
-alias gco='git checkout'
-alias gcob='git checkout -b'
-alias gd='git diff'
-alias gds='git diff --staged'
-alias gdn='git diff --name-only'
-alias gf='git fetch'
-alias gl='git l'
-alias glg='git lg'
-alias gm='git merge'
-alias gcp='git cherrypick'
-alias grb='git rebase'
-alias gph='git push'
-alias gpl='git pull'
-alias gr='git reset'
-alias grs='git restore'
-alias gsh='git stash'
-alias gs='git status'
 
-gsmr() {
-  git submodule deinit -f $1
-  git rm -f $1
-  rm -rf .git/modules/$1
+eval `git alias | awk '
+function join(array, start, end, sep,    result, i)
+{
+    if (sep == "")
+       sep = " "
+    else if (sep == SUBSEP) # magic value
+       sep = ""
+    result = array[start]
+    for (i = start + 1; i <= end; i++)
+        result = result sep array[i]
+    return result
 }
 
-gis() {
-  if [ "$1" ]; then
-    gh issue view --web $1
-  else
-    gh issue view --web \`git branch --show-current | grep -o '\#[0-9]\+'\`
-  fi
-}
-
-alias gpr="gh pr view --web >/dev/null 2>&1 || gh pr create --web --assignee @me"
-
-alias gre="gh repo view --web"
+{
+  split($0,a," ");
+  if (length(a) > 5) {
+    print "alias g" a[1] "=git " a[1]
+  } else if (length(a) >= 3) {
+    print "alias g" a[1] "=\"git " join(a, 3, length(a), " ", cmd) "\""
+  }
+}'`
