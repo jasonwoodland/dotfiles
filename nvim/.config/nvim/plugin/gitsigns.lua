94a4b43fda6a6ba3f9f373b7bca0da0889dae317
Jason Woodland <me@jasonwoodland.com>
2021-10-29 11:29:58 +1100

    Show blame 500ms after cursor hold

--- nvim/.config/nvim/plugin/gitsigns.lua
+++ nvim/.config/nvim/plugin/gitsigns.lua
@@ -41,7 +41,7 @@ require('gitsigns').setup {
   current_line_blame_opts = {
     virt_text = true,
     virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
-    delay = 1000,
+    delay = 500,
   },
   current_line_blame_formatter_opts = {
     relative_time = false
