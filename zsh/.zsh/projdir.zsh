f2cc003c23cc202ffe6544d54ae798d413783426
Jason Woodland <me@jasonwoodland.com>
2021-11-05 09:12:04 +1100

    update projdir

--- zsh/.zsh/projdir.zsh
+++ zsh/.zsh/projdir.zsh
@@ -1,19 +1,14 @@
 _projdirs=(
-  ~/ispec/*/
-  ~/ispec/*/go/
-  ~/ispec/*/*/apps/*/
-  ~/Projects/*/
-  ~/Projects/strivellc/
-  ~/Projects/strivellc/*/
-  ~/.dotfiles/
-  ~/.dotfiles/*/
+  ~/github.com/*/*
+  ~/github.com/ispec-inc/*/go/
+  ~/github.com/ispec-inc/*/*/apps/*/
 )
 _projdirs=("${(u)_projdirs[@]}") #`
 projdir() {
   if [[ $# -eq 1 ]]; then
     selected=$1
   else
-    selected=`printf "%s\n" "${_projdirs[@]}" | sort | fzf --height 40%`
+    selected=`printf "%s\n" "${_projdirs[@]}" | sort | fzf`
     if [[ $selected == '' ]]; then
       return
     fi
@@ -22,27 +17,55 @@ projdir() {
   dirname=${dirname##.}
   cd $selected
 }
+tprojdir() {
+  if [[ $# -eq 1 ]]; then
+    selected=$1
+  else
+    selected=`printf "%s\n" "${_projdirs[@]}" | sort | fzf`
+    if [[ $selected == '' ]]; then
+      return
+    fi
+  fi
+  dirname=`basename $selected`
+  dirname=${dirname##.}
+
+  if [[ -v TMUX ]]; then
+    tmux switch-client -t $dirname 2>/dev/null
+    if [[ $? -eq 0 ]]; then
+      return
+    fi
+  fi
+
+  tmux attach -t $dirname 2>/dev/null
+  if [[ $? -eq 0 ]]; then
+    return
+  fi
+
+  tmux new-session -s $dirname -d -c $selected 2>/dev/null
+  if [[ $? -eq 0 ]]; then
+    if [[ $2 != "" ]]; then
+      tmux send-keys -t $dirname "$2" Enter
+    fi
+    if [[ -v TMUX ]]; then
+      tmux switch-client -t $dirname 2>/dev/null
+    else
+      tmux attach -t $dirname
+    fi
+  fi
+  zle reset-prompt
+}
 zprojdir() {
   projdir
   zle reset-prompt
 }
 _projdir() {
-  _projdirs=(
-    ~/ispec/*/
-    ~/ispec/*/go/
-    ~/ispec/*/*/apps/*/
-    ~/Projects/*/
-    ~/Projects/strivellc/
-    ~/Projects/strivellc/*/
-    ~/.dotfiles/
-    ~/.dotfiles/*/
-  )
-  _projdirs=("${(u)_projdirs[@]}") #`
   _describe 'parameters' _projdirs
   return
 }
 zle -N zprojdir
 bindkey -a '^[p' zprojdir
 bindkey '^[p' zprojdir
+bindkey -as '^[]' 'itprojdir\n'
+bindkey -s '^[]' '^utprojdir\n'
 alias p='projdir'
 compdef _projdir projdir
