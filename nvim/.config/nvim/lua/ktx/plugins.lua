4a681126f06de658112358c4724d96a2f731068c
Jason Woodland <me@jasonwoodland.com>
2021-11-07 09:10:14 +1100

    nvim add cmp-cmdline

--- nvim/.config/nvim/lua/ktx/plugins.lua
+++ nvim/.config/nvim/lua/ktx/plugins.lua
@@ -14,17 +14,16 @@ require('packer').startup(function()
   use 'jose-elias-alvarez/null-ls.nvim'
   use 'lewis6991/gitsigns.nvim'
 
-  use 'hrsh7th/nvim-cmp'
   use 'hrsh7th/cmp-buffer'
   use 'hrsh7th/cmp-path'
   use 'hrsh7th/cmp-nvim-lua'
   use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/cmp-cmdline'
   use 'petertriho/cmp-git'
-  use 'saadparwaiz1/cmp_luasnip'
-  use 'tamago324/cmp-zsh'
+  use 'hrsh7th/nvim-cmp'
+
   use 'SirVer/ultisnips'
   use 'quangnguyen30192/cmp-nvim-ultisnips'
-
   use 'jose-elias-alvarez/nvim-lsp-ts-utils'
   use 'windwp/nvim-ts-autotag'
 
