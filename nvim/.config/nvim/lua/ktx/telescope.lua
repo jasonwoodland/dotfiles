57a50be6deb8ccb23f3eebee3ea0d343854b2fd9
Jason Woodland <me@jasonwoodland.com>
2021-11-28 11:29:51 +1100

    telescope update dotfiles reop location

--- nvim/.config/nvim/lua/ktx/telescope.lua
+++ nvim/.config/nvim/lua/ktx/telescope.lua
@@ -2,7 +2,7 @@ local M = {}
 
 function M.dotfiles()
   require('telescope.builtin').find_files {
-    cwd = '~/.dotfiles',
+    cwd = '~/github.com/jasonwoodland/dotfiles',
     hidden = true,
   }
 end
