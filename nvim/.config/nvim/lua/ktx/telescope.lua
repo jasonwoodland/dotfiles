072364627c6da6229834c4ddee6074c078a09137
Jason Woodland <me@jasonwoodland.com>
2022-09-03 11:07:30 +1000

    nvim: telescope project picker

--- nvim/.config/nvim/lua/ktx/telescope.lua
+++ nvim/.config/nvim/lua/ktx/telescope.lua
@@ -1,3 +1,9 @@
+local pickers = require "telescope.pickers"
+local previewers = require "telescope.previewers"
+local finders = require "telescope.finders"
+local conf = require("telescope.config").values
+local action_state = require "telescope.actions.state"
+
 local M = {}
 
 function M.dotfiles()
@@ -6,6 +12,84 @@ function M.dotfiles()
     hidden = true,
     prompt_title = "Dotfiles"
   }
+end
+
+local actions = require "telescope.actions"
+
+local function project_files(cwd)
+  require('telescope.builtin').find_files {
+    cwd = cwd,
+    attach_mappings = function(prompt_bufnr)
+      actions.select_default:replace(function()
+        actions.close(prompt_bufnr)
+        local path = action_state.get_selected_entry()[1]
+        vim.api.nvim_command("edit " .. path)
+        vim.api.nvim_command("cd " .. path)
+      end)
+      actions.select_horizontal:replace(function()
+        actions.close(prompt_bufnr)
+        local path = action_state.get_selected_entry()[1]
+        vim.api.nvim_command("split " .. path)
+        vim.api.nvim_command("cd " .. path)
+      end)
+      actions.select_vertical:replace(function()
+        actions.close(prompt_bufnr)
+        local path = action_state.get_selected_entry()[1]
+        vim.api.nvim_command("vertical split " .. path)
+        vim.api.nvim_command("cd " .. path)
+      end)
+      return true
+    end,
+  }
+end
+
+function M.projects(opts)
+  opts = opts or {}
+  vim.fn.jobstart("ls -1d ~/ghq/*/*/*/ ~/ghq/*/*/dotfiles/*/", {
+    stdout_buffered = true,
+    on_stdout = function(_, data)
+      pickers.new(opts, {
+        prompt_title = "Projects",
+        finder = finders.new_table {
+          results = data,
+        },
+        attach_mappings = function(prompt_bufnr, map)
+          actions.select_default:replace(function()
+            actions.close(prompt_bufnr)
+            local path = action_state.get_selected_entry()[1]
+            vim.api.nvim_command("edit " .. path)
+            vim.api.nvim_command("cd " .. path)
+          end)
+          actions.select_horizontal:replace(function()
+            actions.close(prompt_bufnr)
+            local path = action_state.get_selected_entry()[1]
+            vim.api.nvim_command("split " .. path)
+            vim.api.nvim_command("cd " .. path)
+          end)
+          actions.select_vertical:replace(function()
+            actions.close(prompt_bufnr)
+            local path = action_state.get_selected_entry()[1]
+            vim.api.nvim_command("vertical split " .. path)
+            vim.api.nvim_command("cd " .. path)
+          end)
+          actions.select_tab:replace(function()
+            actions.close(prompt_bufnr)
+            local path = action_state.get_selected_entry()[1]
+            vim.api.nvim_command("tabedit " .. path)
+            vim.api.nvim_command("cd " .. path)
+          end)
+          map("i", "<tab>", function()
+            actions.close(prompt_bufnr)
+            local path = action_state.get_selected_entry()[1]
+            project_files(path)
+          end)
+          return true
+        end,
+        previewer = previewers.vim_buffer_cat.new(opts),
+        sorter = conf.generic_sorter(opts),
+      }):find()
+    end
+  })
 end
 
 return M
--- nvim/.config/nvim/plugin/mappings.vim
+++ nvim/.config/nvim/plugin/mappings.vim
@@ -2,7 +2,6 @@ nnoremap <space>f  <cmd>lua require('telescope.builtin').find_files()<cr>
 nnoremap <space>g  <cmd>lua require('telescope.builtin').live_grep()<cr>
 nnoremap <space>r  <cmd>lua require('telescope.builtin').resume()<cr>
 nnoremap <space>o  <cmd>lua require('telescope.builtin').oldfiles()<cr>
-nnoremap <space>m  <cmd>lua require('telescope').extensions.frecency.frecency()<cr>
 nnoremap <space>ic <cmd>lua require('telescope.builtin').git_commits()<cr>
 nnoremap <space>ib <cmd>lua require('telescope.builtin').git_branches()<cr>
 nnoremap <space>is <cmd>lua require('telescope.builtin').git_status()<cr>
@@ -10,7 +9,8 @@ nnoremap <space>ih <cmd>lua require('telescope.builtin').git_stash()<cr>
 nnoremap <space>ii <cmd>lua require('telescope').extensions.gh.issues()<cr>
 nnoremap <space>ia <cmd>lua require('telescope').extensions.gh.issues({ assignee="jasonwoodland" })<cr>
 nnoremap <space>ip <cmd>lua require('telescope').extensions.gh.pull_request()<cr>
-nnoremap <space>df <cmd>lua require('ktx.telescope').dotfiles()<cr>
+nnoremap <space>d <cmd>lua require('ktx.telescope').dotfiles()<cr>
+nnoremap <space>p <cmd>lua require('ktx.telescope').projects()<cr>
 
 nnoremap <space>e <cmd>NvimTreeToggle<cr>
 
