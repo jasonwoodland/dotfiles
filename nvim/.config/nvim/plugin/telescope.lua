d805a3c67287212ae79ac018f1d46cd4922365ba
Jason Woodland <me@jasonwoodland.com>
2022-09-03 11:08:33 +1000

    nvim: cleanup

--- nvim/.config/nvim/plugin/telescope.lua
+++ nvim/.config/nvim/plugin/telescope.lua
@@ -1,8 +1,7 @@
-local actions = require("telescope.actions")
 require("telescope").setup({
   defaults = {
     file_ignore_patterns = { "%.git/", "node_modules/", "__generated__/", ".npm", ".next" },
-    winblend = 20,
+    winblend = 10,
     mappings = {
       i = {
         ["<C-a>"] = function()
@@ -24,14 +23,8 @@ require("telescope").setup({
       hidden = true,
     },
   },
-  extensions = {
-    -- Your extension configuration goes here:
-    -- extension_name = {
-    --   extension_config_key = value,
-    -- }
-    -- please take a look at the readme of the extension you want to configure
-  },
 })
+
 require("telescope").load_extension("fzf")
 require("telescope").load_extension("gh")
 require("telescope").load_extension("repo")
