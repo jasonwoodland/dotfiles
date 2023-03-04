6b4d5b110323070ddac62fd5ad516f42ad0092b5
Jason Woodland <me@jasonwoodland.com>
2023-03-04 18:42:13 +1100

    nvim: disable treeshitter indent

--- nvim/.config/nvim/plugin/treesitter.lua
+++ nvim/.config/nvim/plugin/treesitter.lua
@@ -32,7 +32,7 @@ require 'nvim-treesitter.configs'.setup {
 		additional_vim_regex_highlighting = false,
 	},
 	indent = {
-		enable = true,
+		enable = false,
 	},
 	-- textobjects = {
 	-- 	select = {
