53a5bcbeff99b49b68db53d20ea19edbb694e4ee
Jason Woodland <me@jasonwoodland.com>
2023-07-09 08:00:00 +0000

    nvim: fix current line blame

--- nvim/.config/nvim/plugin/gitsigns.lua
+++ nvim/.config/nvim/plugin/gitsigns.lua
@@ -15,14 +15,14 @@ require('gitsigns').setup {
 		follow_files = true
 	},
 	attach_to_untracked          = true,
-	current_line_blame           = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+	current_line_blame           = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
 	current_line_blame_opts      = {
 		virt_text = true,
 		virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
-		delay = 1000,
+		delay = 1,
 		ignore_whitespace = false,
 	},
-	current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
+	current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
 	sign_priority                = 6,
 	update_debounce              = 100,
 	status_formatter             = nil, -- Use default
