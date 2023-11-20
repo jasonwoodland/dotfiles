cbde4e192ada3d9922bf2b8870c94fa8b36bf845
Jason Woodland <me@jasonwoodland.com>
2023-11-20 23:57:18 +1100

    nvim: add oil.nvim

---
+++ nvim/.config/nvim/plugin/oil.lua
@@ -0,0 +1,9 @@
+require("oil").setup({
+  default_file_explorer = true,
+  win_options = {
+    number = false,
+  },
+  delete_to_trash = true,
+  skip_confirm_for_simple_edits = true,
+  trash_command = "trash",
+})
