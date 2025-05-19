489bb443ef9d5c704d4fe23112b173496413c9de
Jason Woodland <me@jasonwoodland.com>
2025-05-19 17:47:40 +1000

    zsh: kubectl aliases

---
+++ zsh/.zsh/kubectl-alias.zsh
@@ -0,0 +1,36 @@
+alias k='kubectl'
+
+alias ka="k apply"
+alias kaf="k apply -f"
+alias kak="k apply -k"
+
+alias kapi="k api-resources"
+
+alias kccc="k config current-context"
+alias kcgc="k config get-contexts"
+alias kcsc="k config set-context"
+alias kcuc="k config use-context"
+
+alias kd="k describe"
+
+alias krm="k delete"
+alias krmk="k delete -k"
+
+alias kev="k events"
+
+alias kex="k explain"
+
+alias kexec="k exec -it"
+
+alias kg="k get"
+alias kga="k get all"
+alias kgcm="k get cm"
+alias kgd="k get deploy"
+alias kgi="k get ing"
+alias kgn="k get nodes"
+alias kgp="k get po"
+alias kgs="k get svc"
+
+alias kl="k logs"
+
+alias kpf="k port-forward"
