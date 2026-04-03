2f554ba4d753414270256b7dccbd1befa4a599c9
Jason Woodland <me@jasonwoodland.com>
2026-04-03 11:51:42 +1100

    nvim: rewrite telescope config

--- nvim/.config/nvim/lua/ktx/plugins/telescope.lua
+++ nvim/.config/nvim/lua/ktx/plugins/telescope.lua
@@ -1,185 +1,217 @@
 return {
-  {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.4',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    config = function()
-      local function noremap(mode, lhs, rhs, opts)
-        opts = vim.tbl_extend("force", { noremap = true }, opts or {})
-        vim.keymap.set(mode, lhs, rhs, opts)
-      end
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.4",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			local function noremap(mode, lhs, rhs, opts)
+				opts = vim.tbl_extend("force", { noremap = true }, opts or {})
+				vim.keymap.set(mode, lhs, rhs, opts)
+			end
 
-      local pickers = require "telescope.pickers"
-      local previewers = require "telescope.previewers"
-      local finders = require "telescope.finders"
-      local conf = require("telescope.config").values
-      local action_state = require "telescope.actions.state"
+			local pickers = require("telescope.pickers")
+			local previewers = require("telescope.previewers")
+			local finders = require("telescope.finders")
+			local conf = require("telescope.config").values
+			local action_state = require("telescope.actions.state")
 
-      function dotfiles()
-        require('telescope.builtin').find_files {
-          cwd = '~/ghq/github.com/jasonwoodland/dotfiles',
-          hidden = true,
-          prompt_title = "Dotfiles"
-        }
-      end
+			function dotfiles()
+				require("telescope.builtin").find_files({
+					cwd = "~/Developer/github.com/jasonwoodland/dotfiles",
+					hidden = true,
+					prompt_title = "Dotfiles",
+				})
+			end
 
-      local actions = require "telescope.actions"
+			local actions = require("telescope.actions")
 
-      local function project_files(cwd)
-        require('telescope.builtin').find_files {
-          cwd = cwd,
-          attach_mappings = function(prompt_bufnr)
-            actions.select_default:replace(function()
-              actions.close(prompt_bufnr)
-              local path = action_state.get_selected_entry()[1]
-              vim.api.nvim_command("lcd " .. cwd)
-              vim.api.nvim_command("edit " .. path)
-            end)
-            actions.select_horizontal:replace(function()
-              actions.close(prompt_bufnr)
-              local path = action_state.get_selected_entry()[1]
-              vim.api.nvim_command("lcd " .. cwd)
-              vim.api.nvim_command("split " .. path)
-            end)
-            actions.select_vertical:replace(function()
-              actions.close(prompt_bufnr)
-              local path = action_state.get_selected_entry()[1]
-              vim.api.nvim_command("lcd " .. cwd)
-              vim.api.nvim_command("vertical split " .. path)
-            end)
-            return true
-          end,
-        }
-      end
+			local function project_files(cwd)
+				require("telescope.builtin").find_files({
+					cwd = cwd,
+					attach_mappings = function(prompt_bufnr)
+						actions.select_default:replace(function()
+							actions.close(prompt_bufnr)
+							local path = action_state.get_selected_entry()[1]
+							vim.api.nvim_command("lcd " .. cwd)
+							vim.api.nvim_command("edit " .. path)
+						end)
+						actions.select_horizontal:replace(function()
+							actions.close(prompt_bufnr)
+							local path = action_state.get_selected_entry()[1]
+							vim.api.nvim_command("lcd " .. cwd)
+							vim.api.nvim_command("split " .. path)
+						end)
+						actions.select_vertical:replace(function()
+							actions.close(prompt_bufnr)
+							local path = action_state.get_selected_entry()[1]
+							vim.api.nvim_command("lcd " .. cwd)
+							vim.api.nvim_command("vertical split " .. path)
+						end)
+						return true
+					end,
+				})
+			end
 
-      local function projects(opts)
-        opts = opts or {}
-        local command = "ls -1d ~/ghq/*/*/*/{,server,client} ~/ghq/*/jasonwoodland/dotfiles/*/"
-        local handle = io.popen(command)
+			local function projects(opts)
+				opts = opts or {}
+				local command = "ls -1d ~/Developer/*/*/*/ ~/Developer/*/jasonwoodland/dotfiles/*/"
+				local handle = io.popen(command)
 
-        if (handle == nil) then
-          print('could not run specified command:' .. command)
-          return
-        end
+				if handle == nil then
+					print("could not run specified command:" .. command)
+					return
+				end
 
-        local result = handle:read("a")
+				local result = handle:read("a")
 
-        handle:close()
+				handle:close()
 
-        local files = {}
+				local files = {}
 
-        for token in string.gmatch(result, "[^%c]+") do
-          table.insert(files, token)
-        end
-        pickers.new({
-          prompt_title = "Projects",
-          finder = finders.new_table {
-            results = files,
-          },
-          attach_mappings = function(prompt_bufnr, map)
-            actions.select_default:replace(function()
-              actions.close(prompt_bufnr)
-              local path = action_state.get_selected_entry()[1]
-              vim.api.nvim_command("silent! lcd " .. path)
-              vim.api.nvim_command("edit .")
-            end)
-            actions.select_horizontal:replace(function()
-              actions.close(prompt_bufnr)
-              local path = action_state.get_selected_entry()[1]
-              vim.api.nvim_command("split " .. path)
-              vim.api.nvim_command("lcd " .. path)
-            end)
-            actions.select_vertical:replace(function()
-              actions.close(prompt_bufnr)
-              local path = action_state.get_selected_entry()[1]
-              vim.api.nvim_command("vertical split " .. path)
-              vim.api.nvim_command("lcd " .. path)
-            end)
-            actions.select_tab:replace(function()
-              actions.close(prompt_bufnr)
-              local path = action_state.get_selected_entry()[1]
-              vim.api.nvim_command("tabnew")
-              vim.api.nvim_command("lcd " .. path)
-              vim.api.nvim_command("edit .")
-            end)
-            map("i", "<tab>", function()
-              actions.close(prompt_bufnr)
-              local path = action_state.get_selected_entry()[1]
-              project_files(path)
-            end)
-            return true
-          end,
-          -- previewer = previewers.vim_buffer_cat.new(opts),
-          sorter = conf.generic_sorter(opts),
-        }):find()
-      end
+				for token in string.gmatch(result, "[^%c]+") do
+					table.insert(files, token)
+				end
+				pickers
+					.new({
+						prompt_title = "Projects",
+						finder = finders.new_table({
+							results = files,
+						}),
+						attach_mappings = function(prompt_bufnr, map)
+							actions.select_default:replace(function()
+								actions.close(prompt_bufnr)
+								local path = action_state.get_selected_entry()[1]
+								vim.api.nvim_command("silent! lcd " .. path)
+								vim.api.nvim_command("edit .")
+							end)
+							actions.select_horizontal:replace(function()
+								actions.close(prompt_bufnr)
+								local path = action_state.get_selected_entry()[1]
+								vim.api.nvim_command("split " .. path)
+								vim.api.nvim_command("lcd " .. path)
+							end)
+							actions.select_vertical:replace(function()
+								actions.close(prompt_bufnr)
+								local path = action_state.get_selected_entry()[1]
+								vim.api.nvim_command("vertical split " .. path)
+								vim.api.nvim_command("lcd " .. path)
+							end)
+							actions.select_tab:replace(function()
+								actions.close(prompt_bufnr)
+								local path = action_state.get_selected_entry()[1]
+								vim.api.nvim_command("tabnew")
+								vim.api.nvim_command("lcd " .. path)
+								vim.api.nvim_command("edit .")
+							end)
+							map("i", "<tab>", function()
+								actions.close(prompt_bufnr)
+								local path = action_state.get_selected_entry()[1]
+								project_files(path)
+							end)
+							return true
+						end,
+						-- previewer = previewers.vim_buffer_cat.new(opts),
+						sorter = conf.generic_sorter(opts),
+					})
+					:find()
+			end
 
-      function terminal_buffers()
-        -- Load builtin and configure it
-        local builtin = require('telescope.builtin')
-        local opts = {
-          sort_lastused = true,
-          theme = 'dropdown',
-          previewer = true,
-          -- your other desired options here
-        }
+			function terminal_buffers()
+				-- Load builtin and configure it
+				local builtin = require("telescope.builtin")
+				local opts = {
+					sort_lastused = true,
+					theme = "dropdown",
+					previewer = true,
+					-- your other desired options here
+				}
 
-        -- builtin.buffers(opts)
-        local pickers = require('telescope.pickers')
-        local finders = require('telescope.finders')
-        local sorters = require('telescope.sorters')
-        local actions = require('telescope.actions')
+				-- builtin.buffers(opts)
+				local pickers = require("telescope.pickers")
+				local finders = require("telescope.finders")
+				local sorters = require("telescope.sorters")
+				local actions = require("telescope.actions")
 
+				local terminal_buffers = vim.tbl_filter(function(val)
+					return vim.fn.getbufvar(val.bufnr, "&buftype") == "terminal"
+				end, vim.fn.getbufinfo({ buflisted = 1 }))
 
-        local terminal_buffers = vim.tbl_filter(
-          function(val)
-            return vim.fn.getbufvar(val.bufnr, "&buftype") == "terminal"
-          end,
-          vim.fn.getbufinfo({ buflisted = 1 })
-        )
+				local function entry_maker(entry)
+					return {
+						value = entry.bufnr,
+						ordinal = entry.name,
+						display = entry.name,
+					}
+				end
 
-        local function entry_maker(entry)
-          return {
-            value = entry.bufnr,
-            ordinal = entry.name,
-            display = entry.name,
-          }
-        end
+				require("telescope.builtin").buffers({
+					prompt_title = "Terminal Buffers",
+					previewer = true,
+					results = terminal_buffers,
+				})
+			end
 
+			noremap("n", "<space>f", require("telescope.builtin").find_files)
+			noremap("n", "<space>g", require("telescope.builtin").live_grep)
+			noremap("n", "<space>r", require("telescope.builtin").resume)
+			noremap("n", "<space>o", require("telescope.builtin").oldfiles)
+			noremap("n", "<space>b", require("telescope.builtin").buffers)
+			noremap("n", "<space>c", require("telescope.builtin").colorscheme)
+			noremap("n", "<space>ic", require("telescope.builtin").git_commits)
+			noremap("n", "<space>ib", require("telescope.builtin").git_branches)
+			noremap("n", "<space>is", require("telescope.builtin").git_status)
+			noremap("n", "<space>ih", require("telescope.builtin").git_stash)
+			noremap("n", "<space>ii", require("telescope").extensions.gh.issues)
+			noremap("n", "<space>ia", function()
+				require("telescope").extensions.gh.issues({ assignee = "jasonwoodland" })
+			end)
+			noremap("n", "<space>ip", require("telescope").extensions.gh.pull_request)
+			noremap("n", "<space>d", dotfiles)
+			noremap("n", "<space>p", projects)
+			noremap("n", "<space>t", terminal_buffers)
+			noremap("n", "<space>lr", require("telescope.builtin").lsp_references)
+			noremap("n", "<space>ld", require("telescope.builtin").lsp_definitions)
+			noremap("n", "<space>li", require("telescope.builtin").lsp_implementations)
+			noremap("n", "<space>ls", require("telescope.builtin").lsp_document_symbols)
+			noremap("n", "<space>lt", require("telescope.builtin").lsp_type_definitions)
+			noremap("n", "<space>lw", require("telescope.builtin").lsp_dynamic_workspace_symbols)
+			-- noremap("n", "<space>n", require("telescope.builtin").current_buffer_fuzzy_find)
+			vim.api.nvim_create_autocmd("User", {
+				pattern = "TelescopePreviewerLoaded",
+				callback = function()
+					-- “0” refers to the preview window, so this sets `number` there
+					vim.api.nvim_set_option_value("number", true, { scope = "local" })
+					vim.api.nvim_set_option_value("signcolumn", "yes", { scope = "local" })
+				end,
+			})
+			vim.keymap.set("n", "<space>n", function()
+				local opts = {
+					sorting_strategy = "ascending",
+					results_ts_highlight = true,
+				}
 
-
-        require("telescope.builtin").buffers({
-          prompt_title = 'Terminal Buffers',
-          previewer = true,
-          results = terminal_buffers,
-        })
-      end
-
-      noremap("n", "<space>f", require("telescope.builtin").find_files)
-      noremap("n", "<space>g", require("telescope.builtin").live_grep)
-      noremap("n", "<space>r", require("telescope.builtin").resume)
-      noremap("n", "<space>o", require("telescope.builtin").oldfiles)
-      noremap("n", "<space>b", require("telescope.builtin").buffers)
-      noremap("n", "<space>c", require("telescope.builtin").colorscheme)
-      noremap("n", "<space>ic", require("telescope.builtin").git_commits)
-      noremap("n", "<space>ib", require("telescope.builtin").git_branches)
-      noremap("n", "<space>is", require("telescope.builtin").git_status)
-      noremap("n", "<space>ih", require("telescope.builtin").git_stash)
-      noremap("n", "<space>ii", require("telescope").extensions.gh.issues)
-      noremap("n", "<space>ia", function() require("telescope").extensions.gh.issues({ assignee = "jasonwoodland" }) end)
-      noremap("n", "<space>ip", require("telescope").extensions.gh.pull_request)
-      noremap("n", "<space>d", dotfiles)
-      noremap("n", "<space>p", projects)
-      noremap("n", "<space>t", terminal_buffers)
-      noremap("n", "<space>lr", require("telescope.builtin").lsp_references)
-      noremap("n", "<space>ld", require("telescope.builtin").lsp_definitions)
-      noremap("n", "<space>li", require("telescope.builtin").lsp_implementations)
-      noremap("n", "<space>ls", require("telescope.builtin").lsp_document_symbols)
-      noremap("n", "<space>lt", require("telescope.builtin").lsp_type_definitions)
-      noremap("n", "<space>lw", require("telescope.builtin").lsp_dynamic_workspace_symbols)
-    end
-  },
-  { "nvim-telescope/telescope-github.nvim" },
-  { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-  { "cljoly/telescope-repo.nvim" },
+				-- Override entry_maker so that:
+				-- • `display` is just the raw line text (no "42: foo()")
+				-- • `highlights` comes from opts.line_highlights (built-in)
+				opts.entry_maker = function(entry)
+					local row = entry.lnum
+					return {
+						valid = true,
+						value = entry, -- pressing <CR> jumps to this entry.bufnr,row⋯
+						display = entry.text, -- **no** "23: foo()" prefix, just "foo()"
+						ordinal = entry.text,
+						bufnr = entry.bufnr,
+						lnum = entry.lnum,
+						col = 0,
+						filename = entry.filename,
+						highlights = opts.line_highlights and opts.line_highlights[row] or nil,
+					}
+				end
+				require("telescope.builtin").current_buffer_fuzzy_find(opts)
+			end, { desc = "Telescope: buffer filter (no linenr, TS highlights, prompt bottom)" })
+		end,
+	},
+	{ "nvim-telescope/telescope-github.nvim" },
+	{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+	{ "cljoly/telescope-repo.nvim" },
 }
--- nvim/.config/nvim/plugin/telescope.lua
+++ nvim/.config/nvim/plugin/telescope.lua
@@ -1,44 +1,109 @@
+local action_state = require("telescope.actions.state")
 local actions = require("telescope.actions")
+local builtin = require("telescope.builtin")
+local previewers = require("telescope.previewers")
+
+local show_hidden = false
+local show_ignore = true
+
+local new_maker = function(filepath, bufnr, opts)
+	opts = opts or {}
+	filepath = vim.fn.expand(filepath)
+
+	-- Skip previewing files larger than 100 KB
+	vim.loop.fs_stat(filepath, function(err, stat)
+		if not err and stat and stat.size > 100 * 1024 then
+			-- Do not preview the file
+			return
+		else
+			previewers.buffer_previewer_maker(filepath, bufnr, opts)
+		end
+	end)
+end
 
 require("telescope").setup({
-  defaults = {
-    file_ignore_patterns = { "%.git/", "node_modules/", "__generated__/", "\\.npm" },
-    winblend = 0,
-    mappings = {
-      i = {
-        ["<C-a>"] = false,
-        ["<C-h>"] = "which_key",
-        ["<esc>"] = "close",
-        ["<C-i>"] = actions.cycle_previewers_next,
-        ["<C-o>"] = actions.cycle_previewers_prev,
-      },
-    },
-    layout_config = {
-      vertical = { width = 0.5 },
-    },
-  },
-  pickers = {
-    find_files = {
-      hidden = true,
-    },
-    live_grep = {
-      -- hidden = true,
-      additional_args = function(opts)
-        return {
-          "--hidden",
-          "--pcre2",
-          "--max-filesize=5M",
-          -- "--glob '!*.json",
-        }
-      end,
-    },
-    colorscheme = {
-      enable_preview = true,
-    },
-  },
+	defaults = {
+		buffer_previewer_maker = new_maker,
+		winblend = 0,
+		mappings = {
+			i = {
+				["<C-a>"] = false,
+				["<C-h>"] = "which_key",
+				["<esc>"] = "close",
+				["<M-p>"] = require("telescope.actions.layout").toggle_preview,
+				["<C-.>"] = function(prompt_bufnr)
+					show_hidden = not show_hidden
+					local prompt = require("telescope.actions.state").get_current_line()
+					local picker = action_state.get_current_picker(prompt_bufnr).prompt_title
+					require("telescope.actions").close(prompt_bufnr)
+					if picker:lower():match("find files") then
+						builtin.find_files({ default_text = prompt })
+					elseif picker:lower():match("live grep") then
+						builtin.live_grep({ default_text = prompt })
+					end
+				end,
+				["<C-,>"] = function(prompt_bufnr)
+					show_ignore = not show_ignore
+					local prompt = require("telescope.actions.state").get_current_line()
+					local picker = action_state.get_current_picker(prompt_bufnr).prompt_title
+					require("telescope.actions").close(prompt_bufnr)
+					if picker:lower():match("find files") then
+						builtin.find_files({ default_text = prompt })
+					elseif picker:lower():match("live grep") then
+						builtin.live_grep({ default_text = prompt })
+					end
+				end,
+			},
+		},
+		layout_config = {
+			vertical = { width = 0.5 },
+		},
+	},
+	pickers = {
+		find_files = {
+			find_command = function()
+				local args = {
+					"rg",
+					"--files",
+					"--pcre2",
+				}
+				if show_hidden then
+					table.insert(args, "--hidden")
+				end
+				if show_ignore then
+					table.insert(args, "--no-ignore")
+					table.insert(args, "--no-ignore-parent")
+					table.insert(args, "--ignore-file=.gitignore")
+				else
+				end
+				return args
+			end,
+		},
+		live_grep = {
+			additional_args = function()
+				local args = {
+					"--pcre2",
+					"--max-filesize=1M",
+				}
+				if show_hidden then
+					table.insert(args, "--hidden")
+				end
+				if show_ignore then
+					table.insert(args, "--no-ignore")
+					table.insert(args, "--no-ignore-parent")
+					table.insert(args, "--ignore-file=.gitignore")
+				else
+				end
+				return args
+			end,
+		},
+		colorscheme = {
+			enable_preview = true,
+		},
+	},
 })
 
-require("telescope").load_extension("fzf")
+-- require("telescope").load_extension("fzf")
 require("telescope").load_extension("gh")
 require("telescope").load_extension("repo")
 -- require("telescope").load_extension('harpoon')
