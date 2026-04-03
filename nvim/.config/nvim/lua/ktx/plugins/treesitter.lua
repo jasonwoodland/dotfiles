1365444daec2516641bc207d0f3baf38564593cf
Jason Woodland <me@jasonwoodland.com>
2026-04-03 11:52:00 +1100

    nvim: rewrite treesitter config

--- nvim/.config/nvim/lua/ktx/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/ktx/plugins/treesitter.lua
@@ -1,17 +1,20 @@
 return {
-  { "nvim-treesitter/nvim-treesitter" },
-  {
-    "nvim-treesitter/nvim-treesitter-textobjects",
-    dependencies = { "nvim-treesitter" },
-  },
-  { "JoosepAlviste/nvim-ts-context-commentstring" },
-  {
-    "TXPGhost/nvim-treesitter-context",
-    config = function()
-      require("treesitter-context").setup({
-        multiline_threshold = 1,
-        update_debounce = 100,
-      })
-    end,
-  },
+	{ "nvim-treesitter/nvim-treesitter" },
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		dependencies = { "nvim-treesitter" },
+	},
+	{ "JoosepAlviste/nvim-ts-context-commentstring" },
+	{
+		"TXPGhost/nvim-treesitter-context",
+		config = function()
+			require("treesitter-context").setup({
+				multiline_threshold = 1,
+				update_debounce = 100,
+			})
+			vim.keymap.set("n", "gJ", function()
+				require("treesitter-context").go_to_context(vim.v.count1)
+			end, { silent = true })
+		end,
+	},
 }
