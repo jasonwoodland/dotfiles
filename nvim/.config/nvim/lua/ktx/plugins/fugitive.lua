271b646bd280a92efb64f43f33475f4d66f2773b
Jason Woodland <me@jasonwoodland.com>
2026-04-03 11:47:27 +1100

    nvim: fix fugitive buffer match

--- nvim/.config/nvim/lua/ktx/plugins/fugitive.lua
+++ nvim/.config/nvim/lua/ktx/plugins/fugitive.lua
@@ -6,7 +6,7 @@ return {
         for _, win in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
           local buf = vim.api.nvim_win_get_buf(win)
           local name = vim.api.nvim_buf_get_name(buf)
-          if string.match(name, "%.git//") then
+          if string.match(name, "fugitive:%//") then
             vim.api.nvim_buf_delete(buf, { force = true })
             return
           end
